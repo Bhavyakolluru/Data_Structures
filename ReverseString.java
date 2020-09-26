cbca3c6833f8cf191da7d51881904b70e8be41d0
Bhavyakolluru <55657281+Bhavyakolluru@users.noreply.github.com>
2020-09-26 12:08:37 +0530

    Create ReverseString.java

---
+++ ReverseString.java
@@ -0,0 +1,17 @@
+public class ReverseString   
+{    
+    public static void main(String[] args) {    
+        String string="Java Programming";    
+        //Stores the reverse of given string    
+        String reversedStr = "";    
+            
+        
+        for(int i = string.length()-1; i >= 0; i--){    
+            reversedStr = reversedStr + string.charAt(i);    
+        }    
+            
+        System.out.println("Original string: " + string);    
+           
+        System.out.println("Reverse of given string: " + reversedStr);    
+    }
+}    
